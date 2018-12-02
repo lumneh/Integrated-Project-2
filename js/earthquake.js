cfb0ea3cc40eb586a1a90355ecda76bc32f51ab7
Morema <25913934+Mo-rema07@users.noreply.github.com>
2018-12-02 13:16:11 +0200

    refactored all js into earthquake.js

--- js/earthquake.js
+++ js/earthquake.js
@@ -1,3 +1,14 @@
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 2,
+        center: new google.maps.LatLng(2.8, -187.3),
+        mapTypeId: 'terrain'
+    });
+}
+var thelocation;
+var titleName;
 $(document).ready(
     function(){
         var quakeFeeds = {
